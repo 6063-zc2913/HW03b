42e48cd0cff5d7a5f8b915414596fffabf4e6a7a
Ziyi Chen <zc2913@nyu.edu>
2023-09-22 15:31:22 -0400

    Change

--- sketch.js
+++ sketch.js
@@ -7,10 +7,11 @@ function setup() {
 function draw() {
     background("yellow");
     fill("black");
+    randomSeed(5)
     for(let xpos = 0; xpos <= width; xpos+= eDiam) {
         for(let ypos = 0; ypos <= height; ypos+= eDiam) {
-            randomLength = random() * 25 + 3;
-            randomShift = random() * 12 - 2.5;
+            randomLength = random() * 23 + 3;
+            randomShift = random() * 10 - 2.5;
             ellipse(xpos + randomShift, ypos + randomShift, randomLength, randomLength);
     }
 
